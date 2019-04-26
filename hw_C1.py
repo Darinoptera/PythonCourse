ac0398d5858d23639e06c9fb2689bd87785abb23
Darinoptera <47215217+Darinoptera@users.noreply.github.com>
2019-04-26 15:14:39 +0300

    Update hw_C1.py

--- hw_C1.py
+++ hw_C1.py
@@ -1,8 +1,6 @@
-a=[]
-i=0
-while i!='':
-	a.append(int(i))
-	for j in range (1,len(a)):
-		if int(i)<a[j]:
-			print(a[j])
-	i=input()
+a = [int(i) for i in input('Введите последовательность чисел через пробел: ').split()]
+b=[]
+for i in range (1,len(a)):
+    if a[i-1]<a[i]:
+        b.append(a[i])
+print(b)
